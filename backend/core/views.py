daf9d86a9f7ec6332980109185e407ded1444102
Никита Кузнецов <eudaimonia22@yandex.ru>
2024-07-07 05:09:47 +0700

    fixed favorite get error

--- backend/core/views.py
+++ backend/core/views.py
@@ -57,7 +57,7 @@ def get_recipe_in_favorite_list(user, recipe):
 
 
 def get_recipes_from_favorite_list(recipes, user):
-    return recipes.filter(favorite_recipe__author=user)
+    return recipes.filter(favorite_recipe__user=user)
 
 
 def add_recipe_to_favorite_list(user, recipe):
@@ -188,45 +188,3 @@ def get_short_link(request, pk):
     recipe_url = request.META['HTTP_REFERER']
     surl = get_surl(recipe_url)
     return request.META['HTTP_HOST'] + surl
-
-# Добавленные функции:
-# def get_recipe_in_intermediate_model(intermediate_model, user, recipe):
-#     return intermediate_model.filter(user=user, recipe=recipe)
-
-# def add_recipe_to_intermediate_model(intermediate_model, user, recipe):
-#     return intermediate_model.create(user=user, recipe=recipe)
-
-# def delete_recipe_from_intermediate_model(recipe_in_intermediate_model):
-#     return recipe_in_intermediate_model.delete()
-
-# def add_or_delete_recipe_in_list(
-#         self, request, pk, intermediate_model
-#     ):
-#         user = request.user
-#         recipe = get_recipe_or_404(pk)
-#         recipe_in_intermediate_model = get_recipe_in_intermediate_model(
-#             user=user,
-#             recipe=recipe,
-#             intermediate_model=intermediate_model
-#         )
-#         if request.method == 'POST':
-#             if recipe_in_intermediate_model.exists():
-#                 return Response(
-#                     {'error': const.RECIPE_ALREADY_EXIST_IN_LIST},
-#                     status=status.HTTP_400_BAD_REQUEST)
-#             add_recipe_to_intermediate_model(
-#                 user=user,
-#                 recipe=recipe,
-#                 intermediate_model=intermediate_model
-#             )
-#             serializer = RecipeShortReadSerializer(recipe)
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         if request.method == 'DELETE':
-#             if recipe_in_intermediate_model.exists():
-#                 delete_recipe_from_intermediate_model(
-#                     recipe_in_intermediate_model=recipe_in_intermediate_model
-#                 )
-#                 return Response(status=status.HTTP_204_NO_CONTENT)
-#         return Response(
-#             {'error': const.RECIPE_DOSNT_EXIST_IN_FAVORITE_LIST},
-#             status=status.HTTP_400_BAD_REQUEST)
