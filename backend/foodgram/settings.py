2fd773f60291ee6b95d206d0200bd62ea146d748
Никита Кузнецов <eudaimonia22@yandex.ru>
2024-07-08 19:31:09 +0700

    bug

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', '*') == 'True'
+SECRET_KEY = os.getenv('SECRET_KEY', '*')
 
-DEBUG = os.getenv('DEBUG', '*')
+DEBUG = os.getenv('DEBUG', '*') == 'True'
 # DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(', ')
